7cd826c29a1f8f91d472964676488fc9ec2d5329
Senka <xsenka7@gmail.com>
2025-02-02 15:45:25 +0100

    add view nb employees per service

--- src/lvl_3.sql
+++ src/lvl_3.sql
@@ -167,11 +167,17 @@ WHERE
     c.date_depart IS NULL
 ORDER BY lastname ASC, firstname ASC, login ASC;
 
-
-
-
-
-
+-- Création de la vue view_nb_employees_per_service
+CREATE OR REPLACE VIEW view_nb_employees_per_service AS
+SELECT cs.name_service AS service, COUNT(e.utilisateur_id) AS nb
+FROM
+    contrats_services cs
+    LEFT JOIN contrats c ON cs.name_service = c.service
+    AND c.date_depart IS NULL
+    LEFT JOIN employes e ON c.employe_id = e.utilisateur_id
+GROUP BY
+    cs.name_service
+ORDER BY cs.name_service ASC;
 
 
 -- Procédures lvl_3
