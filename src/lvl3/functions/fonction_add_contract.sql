0b71d0c960f3e408b216e7d7262500ad0a621a4c
Senka <xsenka7@gmail.com>
2025-02-02 15:14:32 +0100

    add fonction add contract

--- src/lvl3/functions/fonction_add_contract.sql
+++ src/lvl3/functions/fonction_add_contract.sql
@@ -1,28 +1,3 @@
--- Fonctions lvl_3  
-
--- Fonction pour ajouter un service à l'entreprise
-CREATE OR REPLACE FUNCTION add_service(
-    name VARCHAR(32),
-    discount INT
-)
-RETURNS BOOLEAN AS $$
-BEGIN
-    IF discount < 0 OR discount > 100 THEN
-        RETURN FALSE;
-    END IF;
-
-    IF EXISTS (SELECT 1 FROM contrats_services WHERE name_service = name) THEN
-        RETURN FALSE;
-    END IF;
-
-    -- Insérer le nouveau service
-    INSERT INTO contrats_services (name_service, reduction_pourcent)
-    VALUES (name, discount);
-
-    RETURN TRUE;
-END;
-$$ LANGUAGE plpgsql;
-
 -- Fonction pour ajouter un contrat de travail
 CREATE OR REPLACE FUNCTION add_contract(
     login VARCHAR(20),
@@ -59,15 +34,3 @@ BEGIN
     RETURN TRUE;
 END;
 $$ LANGUAGE plpgsql;
-
-
-
-
-
-
-
--- Vues lvl_3
-
--- Procédures lvl_3
-
--- Fin lvl_3
