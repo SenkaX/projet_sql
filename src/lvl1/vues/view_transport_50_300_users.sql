431611f34752e5286b6a9b6d3601bcbc0f5f8320
Senka <xsenka7@gmail.com>
2025-02-02 16:34:04 +0100

    remove repo and add lvl2 file

--- src/lvl1/vues/view_transport_50_300_users.sql
+++
@@ -1,7 +0,0 @@
--- Création de la vue view_transport_50_300_users
-CREATE OR REPLACE VIEW view_transport_50_300_users AS
-SELECT nom
-FROM transport
-WHERE
-    capacite_max BETWEEN 50 AND 300
-ORDER BY nom ASC;
--- src/lvl2/vues/view_user_subscription.sql
+++
@@ -1,8 +0,0 @@
--- Création de la vue view_user_subscription
-CREATE OR REPLACE VIEW view_user_subscription AS
-SELECT v.nom || ' ' || v.prenom AS user, f.nom AS offer
-FROM
-    abonnements a
-    JOIN voyageurs v ON a.utilisateur_id = v.id
-    JOIN forfaits f ON a.forfait_id = f.id
-ORDER BY user ASC, offer ASC;
